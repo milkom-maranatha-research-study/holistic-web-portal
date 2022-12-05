9fb13eab08ef8e2abb2cc5f3ac6131abf9d3e251
Giezka Veby Agustin <75057884+Giezkavebya@users.noreply.github.com>
2022-12-05 13:07:23 +0700

    Add FE template

---
+++ src/views/auth/Login.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  return (
+    <>
+      <div className="container mx-auto px-4 h-full">
+        <div className="flex content-center items-center justify-center h-full">
+          <div className="w-full lg:w-4/12 px-4">
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+              <div className="flex-auto px-4 lg:px-10 py-10 pt-10">
+                <form className="py-4">
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Username
+                    </label>
+                    <input
+                      type="text"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="Username"
+                    />
+                  </div>
+
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="Password"
+                    />
+                  </div>
+
+                  <div className="text-center mt-6">
+                    <button
+                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                      type="button"
+                    >
+                      Sign In
+                    </button>
+                  </div>
+                  <div className="mt-3">
+                    <p className="text-center text-sm mb-3">Not registered?</p>
+                      <Link to="/auth/register" className="text-blueGray-600">
+                        <button
+                      className="bg-blueGray-800 text-white hover:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                      type="button"
+                    >
+                      Sign Up
+                    </button>
+                      </Link>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
