4daacd007b75974f75531d18e0e7cba3be2a2435
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-06 15:06:55 +0700

    Reorganize `HTTPRequest` class

--- src/api/common/HTTPRequest.js
+++ src/api/common/HTTPRequest.js
@@ -1,3 +1,4 @@
+import { kase } from 'kase';
 import { NetworkRequestFailedError } from "./Error";
 import { HTTPContentType, HTTPHeader, HTTPHeaderKey } from "./HTTPHeader";
 import { HTTPMethod } from "./HTTPMethod";
@@ -5,53 +6,15 @@ import { HTTPResponse } from "./HTTPResponse";
 
 export class HTTPRequest {
 
-    /**
-     * Build request's header based on that `header` dictionary.
-     * 
-     * Header example:
-     * 
-     * ```
-     * { contentType: "application/json", basicAuth: { username: "username", password: "password" }, token: "token", ... }
-     * ```
-     * 
-     * @param {Map<string, any>} header Header's map object
-     * @returns Object
-     */
-    headerBuilder(header) {
-        return {
-            [HTTPHeaderKey.ContentType]: header.contentType ? header.contentType : HTTPContentType.ApplicationJson,
-            [HTTPHeaderKey.Authorization]: header.basicAuth
-                ? HTTPHeader.getBasicAuthBase64String(header.basicAuth.username, header.basicAuth.password)
-                : header.token
-                ? HTTPHeader.getAuthorizationTokenString(header.token)
-                : undefined,
-            ...header.customHeaders,
-        };
-    }
-
-    /**
-     * Build query parameters based on that `query` dictionary.
-     * 
-     * Query example:
-     * 
-     * ```
-     * { type: "churn_rate", start_date: "2022-12-06", end_date: "2022-12-07", ... }
-     * ```
-     * @param {Map<string, any>} query Query string's map object
-     * @returns String
-     */
-    queryBuilder(query) {
-        const keys = Object.keys(query);
-        const queryValues = keys.filter(key => query[key] !== undefined)
-            .map(key => {
-                const queryValue = query[key];
-                if (queryValue instanceof Array) return `${key}=${queryValue.join(',')}`;
-                if (queryValue === true) return `${key}=${'True'}`;
-                if (queryValue === false) return `${key}=${'False'}`;
-                return `${key}=${queryValue.toString()}`;
-            });
-
-        return queryValues.join('&');
+    constructor () {
+        this.get = this.get.bind(this);
+        this.post = this.post.bind(this);
+        this.put = this.put.bind(this);
+        this.delete = this.delete.bind(this);
+        this._headerBuilder = this._headerBuilder.bind(this);
+        this._queryBuilder = this._queryBuilder.bind(this);
+        this._convertResponseToCamelCase = this._convertResponseToCamelCase.bind(this);
+        this._convertBodyToSnakeCase = this._convertBodyToSnakeCase.bind(this);
     }
 
     /**
@@ -66,7 +29,7 @@ export class HTTPRequest {
      * Query example:
      * 
      * ```
-     * { type: "churn_rate", start_date: "2022-12-06", end_date: "2022-12-07", ... }
+     * { type: "churn_rate", startDate: "2022-12-06", endDate: "2022-12-07", ... }
      * ```
      * @param {string} path Endpoint
      * @param {Map<string, any>} header Header's map object
@@ -74,9 +37,9 @@ export class HTTPRequest {
      * @returns Promise
      */
     get(path, header={}, query={}) {
-        path = path + this.queryBuilder(query)
+        path = path + this._queryBuilder(query)
 
-        const reqHeader = this.headerBuilder({
+        const reqHeader = this._headerBuilder({
             ...header,
             method: HTTPMethod.Get
         })
@@ -86,6 +49,7 @@ export class HTTPRequest {
                 throw new NetworkRequestFailedError({error});
             })
             .then((response) => HTTPResponse.handleResponse(response))
+            .then(jsonBody => this._convertResponseToCamelCase(jsonBody))
             .catch(error => {
                 throw error;
             });
@@ -103,18 +67,18 @@ export class HTTPRequest {
      * Body example:
      * 
      * ```
-     * { type: "churn_rate", start_date: "2022-12-06", end_date: "2022-12-07", value: "10.5", ... }
+     * { type: "churn_rate", startDate: "2022-12-06", endDate: "2022-12-07", value: "10.5", ... }
      * ```
      * @param {string} path Endpoint
      * @param {Map<string, any>} header Header's map object
      * @param {Map<string, any>} query Query string's map object
      * @returns Promise
      */
-    post(path, header={}, body={}) {
-        const reqHeader = this.headerBuilder({
+    post(path, header={}, body) {
+        const reqHeader = this._headerBuilder({
             ...header,
             method: HTTPMethod.Post,
-            body: JSON.stringify(body)
+            body: JSON.stringify(this._convertBodyToSnakeCase(body))
         })
 
         return fetch(path, reqHeader)
@@ -122,6 +86,7 @@ export class HTTPRequest {
                 throw new NetworkRequestFailedError({error});
             })
             .then((response) => HTTPResponse.handleResponse(response))
+            .then(jsonBody => this._convertResponseToCamelCase(jsonBody))
             .catch(error => {
                 throw error;
             });
@@ -139,18 +104,18 @@ export class HTTPRequest {
      * Body example:
      * 
      * ```
-     * { type: "churn_rate", start_date: "2022-12-06", end_date: "2022-12-07", value: "10.5", ... }
+     * { type: "churn_rate", startDate: "2022-12-06", endDate: "2022-12-07", value: "10.5", ... }
      * ```
      * @param {string} path Endpoint
      * @param {Map<string, any>} header Header's map object
      * @param {Map<string, any>} query Query string's map object
      * @returns Promise
      */
-    put(path, header={}, body={}) {
-        const reqHeader = this.headerBuilder({
+    put(path, header={}, body) {
+        const reqHeader = this._headerBuilder({
             ...header,
             method: HTTPMethod.Put,
-            body: JSON.stringify(body)
+            body: JSON.stringify(this._convertBodyToSnakeCase(body))
         })
 
         return fetch(path, reqHeader)
@@ -158,6 +123,7 @@ export class HTTPRequest {
                 throw new NetworkRequestFailedError({error});
             })
             .then((response) => HTTPResponse.handleResponse(response))
+            .then(jsonBody => this._convertResponseToCamelCase(jsonBody))
             .catch(error => {
                 throw error;
             });
@@ -176,7 +142,7 @@ export class HTTPRequest {
      * @returns Promise
      */
     delete(path, header={}) {
-        const reqHeader = this.headerBuilder({
+        const reqHeader = this._headerBuilder({
             ...header,
             method: HTTPMethod.Delete
         })
@@ -186,8 +152,95 @@ export class HTTPRequest {
                 throw new NetworkRequestFailedError({error});
             })
             .then((response) => HTTPResponse.handleResponse(response))
+            .then(jsonBody => this._convertResponseToCamelCase(jsonBody))
             .catch(error => {
                 throw error;
             });
+    }
+
+    /**
+     * Build request's header based on that `header` dictionary.
+     * 
+     * Header example:
+     * 
+     * ```
+     * { contentType: "application/json", basicAuth: { username: "username", password: "password" }, token: "token", ... }
+     * ```
+     * 
+     * @param {Map<string, any>} header Header's map object
+     * @returns Object
+     */
+    _headerBuilder(header={}) {
+        return {
+            [HTTPHeaderKey.ContentType]: header.contentType ? header.contentType : HTTPContentType.ApplicationJson,
+            [HTTPHeaderKey.Authorization]: header.basicAuth
+                ? HTTPHeader.getBasicAuthBase64String(header.basicAuth.username, header.basicAuth.password)
+                : header.token
+                ? HTTPHeader.getAuthorizationTokenString(header.token)
+                : undefined,
+            ...header.customHeaders,
+        };
+    }
+
+    /**
+     * Build query parameters based on that `query` dictionary.
+     * 
+     * Query example:
+     * 
+     * ```
+     * { type: "churn_rate", startDate: "2022-12-06", endDate: "2022-12-07", ... }
+     * ```
+     * @param {Map<string, any>} query Query string's map object
+     * @returns String
+     */
+    _queryBuilder(query={}) {
+        const keys = Object.keys(query);
+        const queryValues = keys.filter(key => query[key] !== undefined)
+            .map(key => {
+                const queryValue = query[key];
+                const snakeCaseKey = kase(key, 'snake');
+
+                if (Array.isArray(queryValue)) return `${snakeCaseKey}=${queryValue.join(',')}`;
+                if (queryValue === true) return `${snakeCaseKey}=${'True'}`;
+                if (queryValue === false) return `${snakeCaseKey}=${'False'}`;
+
+                return `${snakeCaseKey}=${queryValue.toString()}`;
+            });
+
+        return queryValues.join('&');
+    }
+
+    /**
+     * A function to convert `Response` data into camel case style.
+     * 
+     * @param {Object} obj Response data
+     * @returns Object
+     */
+    _convertResponseToCamelCase(obj) {
+        if (!obj) return obj;
+        if (Array.isArray(obj)) return obj.map(item => this._convertResponseToCamelCase(item));
+        if (typeof obj !== 'object' || obj === null) return obj;
+
+        return Object.keys(obj).reduce((accum, key) => {
+            return Object.assign(accum, {
+                [kase(key, 'camel')]: this._convertResponseToCamelCase(obj[key]),
+            });
+        });
+    }
+
+    /**
+     * A function to convert `Request` body into snake case style.
+     * 
+     * @param {Object} obj Request body
+     * @returns Object
+     */
+    _convertBodyToSnakeCase(obj) {
+        if (!obj) return obj;
+        if (Array.isArray(obj)) return obj.map(item => this._convertBodyToSnakeCase(item));
+        if (typeof obj !== 'object' || obj === null) return obj;
+
+        return Object.keys(obj).reduce((accum, key) => {
+            return Object.assign(accum, { [kase(key, 'snake')]: this._convertBodyToSnakeCase(obj[key]) });
+        });
     }
 }
