2552c0f23d5d29e65975a5e02c0a83c315212e56
panjiyudasetya <panjiyudasetya@gmail.com>
2023-01-29 12:12:58 +0700

    Add total therapists data mappers

---
+++ src/data/mappers/TotalTherapist.js
@@ -0,0 +1,13 @@
+
+export function toMonthlyValues(totalTherapists) {
+    const monthlyMap = {
+        0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0,
+        6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0
+    };
+
+    totalTherapists.forEach((item) => {
+        monthlyMap[item.startDate.month()] = item.value
+    });
+
+    return Object.entries(monthlyMap).map(item => item[1]);
+}
